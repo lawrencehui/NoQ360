29b27fdf82124032173103a99b397e338ee50c79
Lawrence Hui <lawrence@sanuker.com>
2016-11-26 23:08:54 +0800

    Added modal layout

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,6 @@
 App.setPreference('Orientation', 'portrait');
 App.setPreference('ios-orientation-iphone', 'portrait');
 App.setPreference('AutoHideSplashScreen' ,'true');
-App.setPreference('FadeSplashScreen', 'true');
-App.setPreference('FadeSplashScreenDuration', '500');
+// App.setPreference('FadeSplashScreen', 'true');
+// App.setPreference('FadeSplashScreenDuration', '500');
 App.accessRule("*");
